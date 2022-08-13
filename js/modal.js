9606323f50ffe10cfbdc1419ec30d1fd5f866dbc
denius97 <denius97@gmail.com>
2022-08-13 23:56:59 +0300

    7w

--- js/modal.js
+++ js/modal.js
@@ -10,7 +10,7 @@
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-    refs.body.classList.toggle('no-scroll');
+    refs.modal.classList.toggle('backdrop--is-hidden');
+    refs.body.classList.toggle('body--no-scroll');
   }
 })();
